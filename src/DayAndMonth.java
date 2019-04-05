f6ad30acd433bf91bf3bd259fb2e26276da60e70
Dante <dante.collier.2022@student.lhschools.org>
2019-04-05 14:18:43 -0400

    Introduction code

---
+++ src/DayAndMonth.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class DayAndMonth {
+    public static void main(String[]args){
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Today I am going to tell you what day you were born.");
+        System.out.println("What month were you born? (Enter a number)");
+        int month = keyboard.nextInt();
+        System.out.println("What date were you born?");
+        int date = keyboard.nextInt();
+        System.out.println("What year were you born?");
+        int year = keyboard.nextInt();
+        }
+    public static void math(int month, int date, int year){
+
+    }
+}
