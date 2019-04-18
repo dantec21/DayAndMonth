4a9d204b2b5ffb7b69922a415e4ccab23747a196
Dante <dante.collier.2022@student.lhschools.org>
2019-04-18 14:18:22 -0400

    Simplified code, (made easier to read)

--- src/DayAndMonth.java
+++ src/DayAndMonth.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class DayAndMonth {
-    public static void main(String[]args){
+    public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
         System.out.println("Today I am going to tell you what day you were born.");
         System.out.println("What month were you born? (Enter a number)");
@@ -9,14 +9,19 @@ public class DayAndMonth {
         System.out.println("What date were you born?");
         int date = keyboard.nextInt();
         System.out.println("What year were you born?");
-        int year = (keyboard.nextInt() % 100);
-        int century = year/100;
-        math(month, date, year, century);
-
-        }
-    public static void math(int month, int date, int year, int century) {
-        int day = (date + ((13 * (month + 1)) / 5) + year + (year / 4) + (century/4) - (2*century))%7;
-        System.out.println(day);
+        int year = keyboard.nextInt();
+        int calcDay = calcDayOfWeek(month, date, year);
+        System.out.println(calcDay);
 
     }
+
+    public static int calcDayOfWeek(int month, int date, int year) {
+        int century = year / 100;
+        int modYear = (year % 100);
+        int day = (date + ((13 * (month + 1)) / 5) + modYear + ((modYear) / 4) + (century / 4) - (2 * century) % 7);
+        return day;
+
+    }
+
 }
+
