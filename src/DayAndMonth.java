63404f38b01035a89135962e9d38eeea4fc36c21
Dante <dante.collier.2022@student.lhschools.org>
2019-04-22 14:07:28 -0400

    Fixed bug in formula

--- src/DayAndMonth.java
+++ src/DayAndMonth.java
@@ -5,20 +5,20 @@ public class DayAndMonth {
         Scanner keyboard = new Scanner(System.in);
         System.out.println("Today I am going to tell you what day you were born.");
         System.out.println("What month were you born? (Enter a number)");
-        int month = keyboard.nextInt();
+        int m = keyboard.nextInt();
         System.out.println("What date were you born?");
-        int date = keyboard.nextInt();
+        int q = keyboard.nextInt();
         System.out.println("What year were you born?");
         int year = keyboard.nextInt();
-        int calcDay = calcDayOfWeek(month, date, year);
+        int calcDay = calcDayOfWeek(m, q, year);
         System.out.println(calcDay);
 
     }
 
-    public static int calcDayOfWeek(int month, int date, int year) {
-        int century = year / 100;
-        int modYear = (year % 100);
-        int day = (date + ((13 * (month + 1)) / 5) + modYear + ((modYear) / 4) + (century / 4) - (2 * century) % 7);
+    public static int calcDayOfWeek(int m, int q, int year) {
+        int J = (year / 100);
+        int K = (year % 100);
+        int day = (q + ((13*(m+1))/5) + K + (K/4) + (J/4) - (2*J))%7;
         return day;
 
     }
