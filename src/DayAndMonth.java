0964cd43d84fb3ffc0b6d1aee6c41614a4b23f84
Dante <dante.collier.2022@student.lhschools.org>
2019-04-26 13:17:48 -0400

    Days in month code finished

--- src/DayAndMonth.java
+++ src/DayAndMonth.java
@@ -7,7 +7,8 @@ public class DayAndMonth {
         while (1==1) {
             System.out.println("What month were you born? (Enter a number)");
             int month = keyboard.nextInt();
-            System.out.println("What date were you born?");
+            daysInMonth(month);
+            System.out.println("What date of the month were you born?");
             int date = keyboard.nextInt();
             System.out.println("What year were you born?");
             int year = keyboard.nextInt();
@@ -17,61 +18,65 @@ public class DayAndMonth {
             calcDayOfWeek(calcDay);
             poem(calcDay);
             }
-
-
-
         }
 
-    public static String daysInMonth(int month){
-        if (month==1){
-            return ("You were born in January" +
-                    "There are 31 days in January");
+    public static boolean daysInMonth(int month){
+       // System.out.println("Inside of the daysInMonth method");
+
+
+        switch (month){
+            case 1:
+                System.out.println("You were born in January");
+                System.out.println("There are 31 days in January");
         }
-        if (month==2){
-            return ("You were born in February" +
-                    "Normally there are 28 days in February");
+        switch (month){
+            case 2:
+            System.out.println ("You were born in February");
+                System.out.println("Normally there are 28 days in February");
         }
-        if (month==3){
-            return ("You were born in March" +
-                    "There are 31 days in March");
+        switch (month){
+            case 3:
+            System.out.println ("You were born in March");
+            System.out.println ("There are 31 days in March");
         }
         if (month==4){
-            return ("");
-
+            System.out.println("You were born in April");
+            System.out.println("There are 30 days in April");
         }
         if (month==5){
-            return ("");
-
+            System.out.println("You were born in May");
+            System.out.println("There are 31 days in May");
         }
         if (month==6){
-            return ("");
-
+            System.out.println("You were born in June");
+            System.out.println("There are 30 days in June.");
         }
         if (month==7){
-            return ("");
-
+            System.out.println("Your were born in July");
+            System.out.println("There are 31 days in July");
         }
         if (month==8){
-            return ("");
-
+            System.out.println("You were born in August");
+            System.out.println("There are 31 days in August");
         }
         if (month==9){
-           return ("");
-
+            System.out.println("Your were born in September");
+            System.out.println("There are 30 days in September");
         }
         if (month==10){
-            return ("");
-
+            System.out.println("You were born in October");
+            System.out.println("There are 31 days in October");
         }
         if (month==11){
-           return ("");
-
+            System.out.println("You were born in November");
+            System.out.println("There are 30 days in November");
         }
         if (month==12){
-            return ("");
-
+            System.out.println("You were born in December");
+            System.out.println("There are 31 days in December");
         }
-        return (" ");
+        return true;
+
     }
 
 
@@ -109,31 +114,36 @@ public class DayAndMonth {
 
 
     }
-    public static String poem(int calcDay){
+    public static boolean poem(int calcDay){
         if (calcDay == 0){
-            return ("Saturday's child works hard for a living");
+            System.out.println("Saturday's child works hard for a living");
         }
-        if (calcDay ==1){
-            return("The child born on the Sabbath Day" +
-                    "Is fair and wise and good in every way");
-
+        switch (calcDay){
+            case 1:
+            System.out.println("The child born on the Sabbath Day");
+            System.out.println("Is fair and wise and good in every way");
         }
-        if (calcDay==2){
-            return("Monday's child is fair of face");
+        switch(calcDay){
+            case 2:
+            System.out.println("Monday's child is fair of face");
         }
-        if (calcDay==3){
-            return("Tuesday's child is full of grace");
+        switch (calcDay){
+            case 3:
+            System.out.println("Tuesday's child is full of grace");
         }
-        if (calcDay==4){
-            return ("Wednesday's child is full of woe");
+        switch (calcDay){
+            case 4:
+            System.out.println("Wednesday's child is full of woe");
         }
-        if (calcDay==5){
-            return ("Thursday's child has far to go");
+        switch (calcDay){
+            case 5:
+            System.out.println("Thursday's child has far to go");
         }
-        if (calcDay==6){
-            return("Friday's child is loving and giving");
+        switch (calcDay) {
+            case 6:
+            System.out.println("Friday's child is loving and giving");
         }
-        return (" ");
+        return true;
     }
 
 
